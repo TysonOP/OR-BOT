1238f978a43c9b5e64ccaa98a96f01836ede62f7
TYSONᴰᴱⱽ <74145740+ThunderTyson@users.noreply.github.com>
2021-04-11 23:11:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
     "token": "",
     "prefix": ".",
-    "owners": ["580381333914648579","561404925540564992"],
-    "host": "lava.danbot.host",
-    "port": "2333",
-    "hostpassw": "DBH"
+    "owners": ["580381333914648579"],
+    "host": "",
+    "port": "",
+    "hostpassw": ""
 }
