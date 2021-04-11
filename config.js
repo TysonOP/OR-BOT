c6c9f8068c1e3a8138c204d838ca8a638f8d64ae
TYSONᴰᴱⱽ <74145740+ThunderTyson@users.noreply.github.com>
2021-04-11 23:06:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 module.exports = {
-    "token": "NzQyNjA5NDE5OTc1NTI0Mzcz.XzIm6g.apeS7KfrgcKQKbJSWmlbXLJPI8k",
-    'youtubeAPI': 'AIzaSyDze0SWhbl_9SL2nzm7rbZ7XlmrVXqWQfU',
+    "token": "",
     "prefix": ".",
     "owners": ["580381333914648579","561404925540564992"],
     "host": "lava.danbot.host",
